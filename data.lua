e16276f4318506878a7a26380be6d8a5067bcd57
Choumiko <ghostface@ghostface.info>
2019-03-06 18:41:48 +0100

    messing with styles

--- data.lua
+++ data.lua
@@ -19,24 +19,12 @@ data.raw["gui-style"].default["auto-trash-table"] =
         parent = "table",
     }
 
-data.raw["gui-style"].default["auto-trash-button"] =
-    {
-        type = "button_style",
-        parent = "button",
-        width = 33,
-        height = 33,
-        top_padding = 6,
-        right_padding = 0,
-        bottom_padding = 0,
-        left_padding = 0,
-    }
-
 data.raw["gui-style"].default["auto-trash-sprite-button"] =
     {
         type = "button_style",
         parent = "button",
-        width = 32,
-        height = 32,
+        width = 36,
+        height = 36,
         top_padding = 0,
         right_padding = 0,
         bottom_padding = 0,
@@ -50,41 +38,28 @@ data.raw["gui-style"].default["auto-trash-sprite-button"] =
         },
     }
 
+data.raw["gui-style"].default["auto-trash-button"] =
+    {
+        type = "button_style",
+        parent = "auto-trash-sprite-button",
+    }
 
 data.raw["gui-style"].default["auto-trash-button-paused"] =
     {
         type = "button_style",
-        parent = "button",
-        width = 33,
-        height = 33,
-        top_padding = 6,
-        right_padding = 0,
-        bottom_padding = 0,
-        left_padding = 0,
+        parent = "auto-trash-sprite-button",
     }
 
 data.raw["gui-style"].default["auto-trash-logistics-button"] =
     {
         type = "button_style",
-        parent = "button",
-        width = 33,
-        height = 33,
-        top_padding = 6,
-        right_padding = 0,
-        bottom_padding = 0,
-        left_padding = 0,
+        parent = "auto-trash-sprite-button",
     }
 
 data.raw["gui-style"].default["auto-trash-logistics-button-paused"] =
     {
         type = "button_style",
-        parent = "button",
-        width = 33,
-        height = 33,
-        top_padding = 6,
-        right_padding = 0,
-        bottom_padding = 0,
-        left_padding = 0,
+        parent = "auto-trash-sprite-button",
     }
 
 data.raw["gui-style"].default["auto-trash-expand-button"] =
@@ -93,25 +68,81 @@ data.raw["gui-style"].default["auto-trash-expand-button"] =
         parent = "button",
         width = 16,
         height = 16,
+        padding = 0
     }
+
+data:extend({
+    {
+      type="sprite",
+      name="autotrash_trash",
+      filename = "__AutoTrash__/graphics/gui2.png",
+      width = 128,
+      height = 128,
+      x = 0,
+      y = 0
+}})
+
+data:extend({
+    {
+      type="sprite",
+      name="autotrash_trash_paused",
+      filename = "__AutoTrash__/graphics/gui2.png",
+      width = 128,
+      height = 128,
+      x = 128,
+      y = 0
+}})
+
+data:extend({
+    {
+      type="sprite",
+      name="autotrash_logistics",
+      filename = "__AutoTrash__/graphics/gui2.png",
+      width = 128,
+      height = 128,
+      x = 0,
+      y = 128
+}})
+
+data:extend({
+    {
+      type="sprite",
+      name="autotrash_logistics_paused",
+      filename = "__AutoTrash__/graphics/gui2.png",
+      width = 128,
+      height = 128,
+      x = 128,
+      y = 128
+}})
+
+data:extend({
+    {
+      type="sprite",
+      name="autotrash_expand",
+      filename = "__core__/graphics/side-menu-icons.png",
+      width = 64,
+      height = 64,
+      x = 0,
+      y = 384
+}})
 
 data:extend({
     {
         type = "custom-input",
         name = "autotrash_pause",
-        key_sequence = "SHIFT + p",
+        key_sequence = "SHIFT + P",
         consuming = "none"
     },
     {
         type = "custom-input",
         name = "autotrash_pause_requests",
-        key_sequence = "SHIFT + o",
+        key_sequence = "SHIFT + O",
         consuming = "none"
     },
     {
         type = "custom-input",
         name = "autotrash_trash_cursor",
-        key_sequence = "SHIFT + t",
+        key_sequence = "SHIFT + T",
         consuming = "none"
     },
 })
